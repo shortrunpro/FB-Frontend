09500366320c98dab4d195a21851d0c356da6d8b
Jake Ballenger <71456963+jballeng@users.noreply.github.com>
2026-06-10 17:22:12 -0400

    Root Layout Cleanup
     - Removed bad prefetch links
     - Removed HtmlLangSetter component entirely

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,6 @@ import './globals.css';
 import { Toaster } from '@medusajs/ui';
 import Head from 'next/head';
 
-import { HtmlLangSetter } from '@/components/atoms/HtmlLangSetter/HtmlLangSetter';
 import { retrieveCart } from '@/lib/data/cart';
 
 import { Providers } from './providers';
@@ -41,13 +40,9 @@ export default async function RootLayout({
 }>) {
   const cart = await retrieveCart();
 
-  const ALGOLIA_APP = process.env.NEXT_PUBLIC_ALGOLIA_ID;
-  // default lang updated by HtmlLangSetter
-  const htmlLang = 'en';
-
   return (
     <html
-      lang={htmlLang}
+      lang={'en'}
       className=""
     >
       <Head>
@@ -69,77 +64,8 @@ export default async function RootLayout({
           rel="dns-prefetch"
           href="https://fonts.googleapis.com"
         />
-        <link
-          rel="preconnect"
-          href="https://i.imgur.com"
-          crossOrigin="anonymous"
-        />
-        <link
-          rel="dns-prefetch"
-          href="https://i.imgur.com"
-        />
-        {ALGOLIA_APP && (
-          <>
-            <link
-              rel="preconnect"
-              href="https://algolia.net"
-              crossOrigin="anonymous"
-            />
-            <link
-              rel="preconnect"
-              href="https://algolianet.com"
-              crossOrigin="anonymous"
-            />
-            <link
-              rel="dns-prefetch"
-              href="https://algolia.net"
-            />
-            <link
-              rel="dns-prefetch"
-              href="https://algolianet.com"
-            />
-          </>
-        )}
-        {/* Image origins for faster LCP */}
-        <link
-          rel="preconnect"
-          href="https://medusa-public-images.s3.eu-west-1.amazonaws.com"
-          crossOrigin="anonymous"
-        />
-        <link
-          rel="dns-prefetch"
-          href="https://medusa-public-images.s3.eu-west-1.amazonaws.com"
-        />
-        <link
-          rel="preconnect"
-          href="https://mercur-connect.s3.eu-central-1.amazonaws.com"
-          crossOrigin="anonymous"
-        />
-        <link
-          rel="dns-prefetch"
-          href="https://mercur-connect.s3.eu-central-1.amazonaws.com"
-        />
-        <link
-          rel="preconnect"
-          href="https://s3.eu-central-1.amazonaws.com"
-          crossOrigin="anonymous"
-        />
-        <link
-          rel="dns-prefetch"
-          href="https://s3.eu-central-1.amazonaws.com"
-        />
-        <link
-          rel="preconnect"
-          href="https://api.mercurjs.com"
-          crossOrigin="anonymous"
-        />
-        <link
-          rel="dns-prefetch"
-          href="https://api.mercurjs.com"
-        />
       </Head>
       <body className={`${funnelDisplay.className} relative bg-primary text-secondary antialiased`}>
-        <HtmlLangSetter />
         <Providers cart={cart}>{children}</Providers>
         <Toaster position="top-right" />
       </body>
