d389e573997ace8548176c96f84c908b80928008
Jake Ballenger <71456963+jballeng@users.noreply.github.com>
2026-06-29 15:19:44 -0400

    Homepage carousel container-columns
     - updates to use container-columns class for the homepage carousel section

--- src/modules/home/sections/HomeImageCarousel/HomeImageCarousel.tsx
+++ src/modules/home/sections/HomeImageCarousel/HomeImageCarousel.tsx
@@ -17,10 +17,10 @@ import { Button } from '@/modules/common/components';
 
 export const HomeImageCarousel = () => {
   return (
-    <div className="home-gradient w-full py-6">
-      <div className="mx-auto flex w-full max-w-[1400px]">
-        <div className="flex w-1/2">
-          <div className="flex flex-col justify-center gap-y-4">
+    <div className="home-gradient w-full py-12">
+      <div className="container-columns flex w-full">
+        <div className="flex w-7/12">
+          <div className="flex w-full flex-col justify-center gap-y-4">
             <div className="flex items-center gap-x-3">
               <div className="flex gap-x-2">
                 <StarSolid />
@@ -31,13 +31,13 @@ export const HomeImageCarousel = () => {
               </div>
               <p className="text-sm/3 font-bold">13k+ PROJECTS SERVED</p>
             </div>
-            <h2 className="flex flex-col text-6xl font-bold">
+            <h2 className="3xl:text-7xl flex flex-col font-bold leading-none lg:text-5xl xl:text-6xl">
               <span>Heavy-Duty Metal</span>
               <span>Support Brackets</span>
             </h2>
             <p className="text-sm/10">100% USA-MADE EVERYTIME</p>
             <div>
-              <Button className="flex items-center gap-x-2 rounded-full font-bold hover:bg-gray-600">
+              <Button className="flex items-center gap-x-2 rounded-full font-bold hover:bg-brand">
                 SHOP NOW{' '}
                 <ArrowRight
                   color="#ffffff"
@@ -47,9 +47,9 @@ export const HomeImageCarousel = () => {
             </div>
           </div>
         </div>
-        <div className="home-swiper flex w-1/2">
+        <div className="home-swiper flex w-5/12 justify-end">
           <Swiper
-            className="ml-0 mr-0 max-w-[780px] rounded-md"
+            className="ml-0 mr-0 max-w-[580px] rounded-md"
             modules={[Navigation, Pagination, Autoplay]}
             pagination={{ clickable: true }}
             autoplay={{ delay: 3000 }}
