92c69ad44c562c96c155af9583f9ee4bce16b8b2
Jake Ballenger <71456963+jballeng@users.noreply.github.com>
2026-07-16 22:28:23 -0400

    FBM-142 Results verbiage fix
     - When there are less hits than the number of hits per page, the message at the top just say the number of results

--- src/modules/search/components/ShowAll/ShowAll.tsx
+++ src/modules/search/components/ShowAll/ShowAll.tsx
@@ -5,16 +5,23 @@ import { InteractiveLink } from '@/modules/common/components';
 
 const ShowAll = () => {
   const { results } = useHits();
+  console.log(results);
   return (
     <Container className="small:flex-row mb-4 flex h-fit items-center justify-evenly gap-2 py-2 sm:flex-row">
       {!results || results.nbHits == 0 ? (
         <Text>No results found.</Text>
       ) : (
         <>
-          <Text className="mb-0">
-            Showing the first {results.hitsPerPage} of {results.nbHits} results
-          </Text>
-          <InteractiveLink href={`/results/${results?.query}`}>View all</InteractiveLink>
+          {results.nbHits < results.hitsPerPage ? (
+            <Text className="mb-0 text-left">{results.nbHits} results</Text>
+          ) : (
+            <>
+              <Text className="mb-0">
+                Showing the first {results.hitsPerPage} of {results.nbHits} results
+              </Text>
+              <InteractiveLink href={`/results/${results?.query}`}>View all</InteractiveLink>
+            </>
+          )}
         </>
       )}
     </Container>
