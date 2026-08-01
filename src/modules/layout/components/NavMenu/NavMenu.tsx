04a96a5e924c53874dcc29261fc2b6f292a4ca9e
Jake Ballenger <71456963+jballeng@users.noreply.github.com>
2026-08-01 01:25:13 -0400

    FBL-28 Navigation Menu Items
     - FBL-29 Unable to select top level category in navigation, so you can't click Counter, Shelving, etc to be taken to top level cateogry
     - FBL-30 Remove background hover color for top level category in navigation & instead, change font color when hovering
     - FBL-31 Need spacing between these top level categories --- right now just looks like a bunch of random words in a line, doesn't give off the vibe of a website menu
     - FBL-13 Menu Sticky Issue
     - FBL-9 No drop downs under Tactical in top menu and it does not take you anywhere
     - FBL-6 Spread out top categories (too close together) - like existing Federal Brace site
     - FBL-4 When hovering & moving over the product categories, the drop downs overlap & show more than one drop down as shown

--- src/modules/layout/components/NavMenu/NavMenu.tsx
+++ src/modules/layout/components/NavMenu/NavMenu.tsx
@@ -5,24 +5,49 @@ import Link from 'next/link';
 import menu from './menu.json';
 
 export const NavMenu = () => {
-  return menu.map(item => (
-    <MenuItem
-      key={item.title}
-      item={item}
-    />
-  ));
-};
-
-const MenuItem = ({ item }: { item: any }) => {
   return (
-    <div className="dropdown dropdown-hover">
-      <div
-        tabIndex={0}
-        role="button"
-        className="rounded-field 3xl:px-3 btn btn-ghost font-bold uppercase text-brand lg:px-1 lg:text-xs/3 xl:text-sm/3 2xl:px-1 2xl:text-base/3"
-      >
-        {item.title}
-      </div>
+    <div className="gap-x- flex gap-x-0.5 min-[1150px]:gap-x-1.5 min-[1200px]:gap-x-3 min-[1250px]:gap-x-4 xl:gap-x-1.5 min-[1300px]:gap-x-2 min-[1325px]:gap-x-3 min-[1350px]:gap-x-3.5 min-[1375px]:gap-x-4 min-[1400px]:gap-x-5 min-[1450px]:gap-x-6 min-[1600px]:gap-x-7">
+      {menu.map(item => (
+        <MenuItem
+          key={item.title}
+          item={item}
+        />
+      ))}
+    </div>
+  );
+};
+interface MenuItemProps {
+  item: {
+    title: string;
+    link: string | null;
+    children: {
+      link: string;
+      title: string;
+    }[];
+  };
+}
+const MenuItem = ({ item }: MenuItemProps) => {
+  return (
+    <div className="dropdown dropdown-end dropdown-bottom dropdown-hover">
+      {item.link ? (
+        <Link
+          href={item.link}
+
+          role="button"
+          className="btn btn-ghost px-1 font-bold uppercase text-brand decoration-brand decoration-2 underline-offset-8 focus-within:underline hover:bg-transparent hover:underline focus:underline focus-visible:underline xl:px-2"
+        >
+          {item.title}
+        </Link>
+      ) : (
+        <div
+          // tabIndex={0}
+          role="button"
+          className="btn btn-ghost px-1 font-bold uppercase text-brand decoration-brand decoration-2 underline-offset-8 hover:bg-transparent hover:underline"
+        >
+          {item.title}
+        </div>
+      )}
+
       {item.children.length > 0 && (
         <ul
           tabIndex={-1}
@@ -31,7 +56,12 @@ const MenuItem = ({ item }: { item: any }) => {
           {item.children.map((child: any) => {
             return (
               <li key={child.title}>
-                <Link href={child.link}>{child.title}</Link>
+                <Link
+                  href={child.link}
+                  className="px-2"
+                >
+                  {child.title}
+                </Link>
               </li>
             );
           })}
--- src/modules/layout/components/Navbar/Navbar.tsx
+++ src/modules/layout/components/Navbar/Navbar.tsx
@@ -4,6 +4,8 @@ import Link from 'next/link';
 import { MobileNavbar } from '../MobileNavbar/MobileNavbar';
 import { NavMenu } from '../NavMenu/NavMenu';
 
+// min-[1100px]:max-w-[237px] min-[1150px]:max-w-[252.8px] xl:max-w-[252.8] min-[1300px]:max-w-[252.8] min-[1400px]:max-w-[284.4px]
+
 export const Navbar = () => {
   return (
     <div className="w-full bg-white py-4">
@@ -11,9 +13,9 @@ export const Navbar = () => {
         <div className="navbar-start w-full lg:hidden">
           <MobileNavbar />
         </div>
-        <div className="navbar-center w-full justify-center lg:navbar-start lg:max-w-[316px]">
+        <div className="navbar-start navbar-center w-full justify-center lg:max-w-[221.2px] min-[1050px]:max-w-[237px] min-[1075px]:max-w-[284.4px] min-[1100px]:max-w-[316px] xl:px-4">
           <Link
-            className="relative aspect-[316/43] w-3/4 max-w-[316px] bg-white lg:w-11/12 3xl:w-full"
+            className="relative aspect-[316/43] h-auto w-full max-w-[316px] bg-white"
             href={'/'}
           >
             <Image
