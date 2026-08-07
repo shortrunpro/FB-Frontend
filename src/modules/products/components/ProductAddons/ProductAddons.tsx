92ef53492113ba3a83ad68e11d95b22de7c7f618
Jake Ballenger <71456963+jballeng@users.noreply.github.com>
2026-08-07 16:36:28 -0400

    Product data and addon spacing
     - FBL-63
     - FBL-53
    #ready

--- src/modules/products/components/ProductAddons/ProductAddons.tsx
+++ src/modules/products/components/ProductAddons/ProductAddons.tsx
@@ -7,7 +7,7 @@ export const ProductAddons = ({ addons }: { addons: ProductAddon }) => {
   return (
     <div className="flex flex-col gap-y-4">
       <h3 className="heading-md">Installation Kits + Product Accessories</h3>
-      <div className="flex flex-col items-center gap-x-4 md:flex-row">
+      <div className="flex flex-col items-center gap-4 md:flex-row">
         {addons.variants.map((v: any) => (
           <div
             key={`addon-${v.id}`}
--- src/modules/products/components/ProductVariantModalTabs/ProductVariantModalTabs.tsx
+++ src/modules/products/components/ProductVariantModalTabs/ProductVariantModalTabs.tsx
@@ -41,19 +41,19 @@ export const ProductVariantModalTabs = ({ variant }: { variant: StoreProductVari
 
                 <Table.Row className="bg-white px-4 odd:bg-gray-100 [&_td:last-child]:w-[1%] [&_td:last-child]:whitespace-nowrap">
                   <Table.Cell>Length</Table.Cell>
-                  <Table.Cell>{variant?.length}</Table.Cell>
+                  <Table.Cell>{variant?.length && variant?.length.toFixed(2)}"</Table.Cell>
                 </Table.Row>
                 <Table.Row className="bg-white px-4 odd:bg-gray-100 [&_td:last-child]:w-[1%] [&_td:last-child]:whitespace-nowrap">
                   <Table.Cell>Width</Table.Cell>
-                  <Table.Cell>{variant?.width}</Table.Cell>
+                  <Table.Cell>{variant?.width && variant?.width.toFixed(2)}"</Table.Cell>
                 </Table.Row>
                 <Table.Row className="bg-white px-4 odd:bg-gray-100 [&_td:last-child]:w-[1%] [&_td:last-child]:whitespace-nowrap">
                   <Table.Cell>Height</Table.Cell>
-                  <Table.Cell>{variant?.height}</Table.Cell>
+                  <Table.Cell>{variant?.height && variant?.height.toFixed(2)}"</Table.Cell>
                 </Table.Row>
                 <Table.Row className="bg-white px-4 odd:bg-gray-100 [&_td:last-child]:w-[1%] [&_td:last-child]:whitespace-nowrap">
                   <Table.Cell>Weight</Table.Cell>
-                  <Table.Cell>{variant?.weight}</Table.Cell>
+                  <Table.Cell>{variant?.weight && variant?.weight.toFixed(2)} lbs</Table.Cell>
                 </Table.Row>
               </Table.Body>
             </Table>
