68c557f1755bb0ea444ed635d37698ee65b576c3
Jake Ballenger <71456963+jballeng@users.noreply.github.com>
2026-08-14 11:00:18 -0400

    Blog Main Image to Content
     - FBL-14 Added blogs main image to the top of the blog content
    #ready

--- src/modules/blog/templates/BlogContent/BlogContent.tsx
+++ src/modules/blog/templates/BlogContent/BlogContent.tsx
@@ -1,9 +1,24 @@
+import Image from 'next/image';
+
 import { ContentParser } from '@/modules/common/components';
 import { Blog } from '@/types/blog';
 
 const BlogContent = ({ data }: { data: Blog }) => {
   return (
     <div className="mx-auto max-w-[900px]">
+      <div
+        data-testid="blog-main-image-container"
+        className=""
+      >
+        <Image
+          src={data.main_image.replace(/.jpg|.jpeg|.png|.JPG|.JPEG|.PNG/g, '.webp')}
+          alt={`${data.title} Main Image`}
+          sizes="100vw"
+          width={1000}
+          height={1000}
+          className="h-auto w-full"
+        />
+      </div>
       <ContentParser content={data.content} />
     </div>
   );
