3b4bce79f69b7937acb3e1d15417ea7cb0cb1a88
Jake Ballenger <71456963+jballeng@users.noreply.github.com>
2026-09-17 13:49:00 -0400

    FBL-89 Frontend File Design
     - Updated file types to use different icons per walters request

--- src/modules/products/components/ProductFiles/ProductFiles.tsx
+++ src/modules/products/components/ProductFiles/ProductFiles.tsx
@@ -1,14 +1,48 @@
 import { DocumentText } from '@medusajs/icons';
 import Link from 'next/link';
+import { LuFile, LuFileCog, LuFilePen, LuFileText } from 'react-icons/lu';
 
 import { ProductFile } from '@/types/product';
 
 export const ProductFiles = ({ files }: { files: ProductFile[] | undefined }) => {
+  const fileIcon = (type: string) => {
+    switch (type) {
+      case 'Spec Sheet':
+        return (
+          <LuFileText
+            color="rgb(234, 179, 8)"
+            size={'1.25em'}
+          />
+        );
+      case 'Installation Guide':
+        return (
+          <LuFileCog
+            color="rgb(234, 179, 8)"
+            size={'1.25em'}
+          />
+        );
+      case 'CSI Specifications':
+        return (
+          <LuFilePen
+            color="rgb(234, 179, 8)"
+            size={'1.25em'}
+          />
+        );
+      default:
+        return (
+          <LuFile
+            color="rgb(234, 179, 8)"
+            size={'1.25em'}
+          />
+        );
+    }
+  };
   return (
     <div className="flex gap-x-4">
       {files &&
         files.length &&
         files.map(file => {
+          let icon = fileIcon(file.type);
           return (
             <Link
               className="flex items-center gap-x-1 font-bold text-black"
@@ -17,11 +51,7 @@ export const ProductFiles = ({ files }: { files: ProductFile[] | undefined }) =>
               target="_blank"
               rel="noopener noreferrer"
             >
-              <DocumentText
-                color="rgb(234, 179, 8)"
-                width={26}
-                height={26}
-              />
+              {icon}
               {file.type}
             </Link>
           );
