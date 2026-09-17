fb87bc2be64d4f2d19ab78cba6114cb4d773ef51
Jake Ballenger <71456963+jballeng@users.noreply.github.com>
2026-09-17 15:46:27 -0400

    FBL-55 Company hover menu
     - Added the hover menu back to the company link in top bar per Walters request

--- src/modules/layout/components/NavLinks/NavLinks.tsx
+++ src/modules/layout/components/NavLinks/NavLinks.tsx
@@ -1,50 +1,60 @@
+'use client';
+
 import Link from 'next/link';
 
 import { CartDrawer } from '@/modules/cart/components';
 import { UserDropdown } from '@/modules/users/components';
 
 import { NavbarSearch } from '../NavbarSearch/NavbarSearch';
+import menuItems from './menu.json';
 
 export const NavLinks = ({ user }: any) => {
+  const closeDropdown = () => {
+    if (typeof window !== 'undefined' && document.activeElement instanceof HTMLElement) {
+      document.activeElement.blur();
+    }
+  };
   const isLoggedIn = Boolean(user);
   return (
     <div className="flex gap-x-4">
       <div className="hidden items-center gap-x-3 lg:flex">
-        <Link
-          prefetch={false}
-          href="/request-quote"
-          className="hover:text-neutral-800"
-        >
-          Request a Quote
-        </Link>
-        <Link
-          prefetch={false}
-          href="/categories/clearance"
-          className="hover:text-neutral-800"
-        >
-          Clearance
-        </Link>
-        <Link
-          prefetch={false}
-          href="/blog"
-          className="hover:text-neutral-800"
-        >
-          Blog
-        </Link>
-        <Link
-          prefetch={false}
-          href="/content/category/company"
-          className="hover:text-neutral-800"
-        >
-          Company
-        </Link>
-        <Link
-          prefetch={false}
-          href="/content/category/resources"
-          className="hover:text-neutral-800"
-        >
-          Resources
-        </Link>
+        {menuItems.map(item =>
+          !item.children ? (
+            <Link
+              prefetch={false}
+              href={item.link}
+              className="hover:text-neutral-800"
+            >
+              {item.title}
+            </Link>
+          ) : (
+            <div className="dropdown dropdown-bottom dropdown-hover">
+              <Link
+                prefetch={false}
+                href={item.link}
+                className="hover:text-neutral-800"
+                onClick={closeDropdown}
+              >
+                {item.title}
+              </Link>
+              <ul className="menu dropdown-content z-50 w-52 rounded-box bg-brand_grey px-2 py-0 text-black shadow-sm">
+                {item.children.map((child: any) => {
+                  return (
+                    <li key={child.title}>
+                      <Link
+                        onClick={closeDropdown}
+                        href={child.link}
+                        className="px-2"
+                      >
+                        {child.title}
+                      </Link>
+                    </li>
+                  );
+                })}
+              </ul>
+            </div>
+          )
+        )}
       </div>
       <div className="flex items-center gap-x-4">
         <UserDropdown isLoggedIn={isLoggedIn} />
