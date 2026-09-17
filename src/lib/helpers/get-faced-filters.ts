e609d87b949ea746e76a2fa25c4f2fba0360c36c
Jake Ballenger <71456963+jballeng@users.noreply.github.com>
2026-09-17 14:46:56 -0400

    FBL-57 Product price filter server error
     - Resolved issue with the price filter on product pages was throwing a server error due to invalid filters being sent to the backend

--- src/lib/helpers/get-faced-filters.ts
+++ src/lib/helpers/get-faced-filters.ts
@@ -91,15 +91,17 @@ export const getFacedFilters = (filters: ReadonlyURLSearchParams): string => {
       }
     }
   }
-
+  const conditionalAnd = (value: string) => {
+    return value.length ? ' AND ' : '';
+  };
   const priceFilter =
     minPrice && maxPrice
-      ? ` AND variants.prices.amount:${minPrice} TO ${maxPrice}`
+      ? `variants.price ${minPrice} TO ${maxPrice}`
       : minPrice
-        ? ` AND variants.prices.amount >= ${minPrice}`
+        ? `variants.price >= ${minPrice}`
         : maxPrice
-          ? ` AND variants.prices.amount <= ${maxPrice}`
+          ? `variants.price <= ${maxPrice}`
           : '';
 
-  return facet + priceFilter + rating;
+  return facet + conditionalAnd(facet) + priceFilter + rating;
 };
